e7512a715cef69642d57a4d900a2c771cd079436
Darian Manzanares <daimonwarlordgaming@gmail.com>
2020-04-13 03:41:31 -0700

    Created Sign In UI and animation

--- js/main.js
+++ js/main.js
@@ -18,3 +18,40 @@ $(function() {
   });
   $('.open-menu').height($(window).height());
 });
+
+var modal = document.getElementById('sign-in-modal');
+
+var modalBtn = document.getElementById('sign-in-btn');
+
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+
+window.addEventListener('click', clickOutside);
+
+function openModal(){
+    modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+function clickOutside(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
